4d7b50434bf0e9f6fe932c9ca0d9e18451739b3e
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-04-17 21:01:42 +0530

    feat(token): method to add beneficiary ✨
    - write a method to add beneficiary that will recieve tokens
    - write test accordingly

--- contracts/XYZToken.sol
+++ contracts/XYZToken.sol
@@ -2,9 +2,32 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract XYZToken is ERC20 {
+contract XYZToken is ERC20, Ownable {
+    // addresses where we will disperse token for 12 months, upto 10
+    address[] private _beneficiaries = new address[](10);
+
     constructor(uint256 initialSupply) ERC20("XYZ Token", "XYZ") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, initialSupply * (10 ^ decimals()));
+    }
+
+    // returns benificiaries that will recieve tokens
+    function beneficiaries() public view returns (address[] memory) {
+        return _beneficiaries;
+    }
+
+    // add beneficiaries
+    function addBenificiaries(address[] memory beneficiaries_)
+        external
+        onlyOwner
+    {
+        require(
+            beneficiaries_.length <= 10,
+            "You can add upto 10 benificiary!"
+        );
+        for (uint256 idx = 0; idx < beneficiaries_.length; idx++) {
+            _beneficiaries[idx] = (beneficiaries_[idx]);
+        }
     }
 }
--- test/XYZToken.ts
+++ test/XYZToken.ts
@@ -3,27 +3,54 @@ import { expect } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
+let xyzToken: Contract;
+
+async function deployContract(name: string, ...args: any) {
+    const Contract = await ethers.getContractFactory(name);
+    const contract = await Contract.deploy(...args);
+    await contract.deployed();
+
+    return contract;
+}
+
 describe("XYZ Token", () => {
-    async function deployToken(initialSupply: any) {
-        const XYZToken = await ethers.getContractFactory("XYZToken");
-        const xyzToken = await XYZToken.deploy(initialSupply);
-        await xyzToken.deployed();
-
-        return xyzToken;
-    }
-
-    let xyzToken: Contract;
     before(async function () {
-        xyzToken = await deployToken(100000000);
+        xyzToken = await deployContract("XYZToken", 100000000);
     })
 
-    // check if total supply is 1 million
-    it("should print the initial token supply", async function () {
-        expect(await xyzToken.totalSupply()).to.equal(100000000);
+    it("should check whether the decimals value is 18", async function () {
+        expect(await xyzToken.decimals()).to.equal(18);
     })
 
-    // check the balnce of owner
-    it("should print the balance of ownwer", async function () {
-        expect(await xyzToken.balanceOf("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")).to.equal(100000000);
+    it("should check the initial token supply is 100000000", async function () {
+        expect(await xyzToken.totalSupply()).to.equal(100000000 * (10 ^ 18));
+    })
+
+    it("should check beneficiaries added succesfully", async function () {
+        await xyzToken.addBenificiaries(["0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199"]);
+        const b = (await xyzToken.beneficiaries())[0];
+        expect(b.toLowerCase()).to.equal("0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199");
+    })
+
+    it("should fail at adding beneficiaries", async function () {
+        let e: any;
+        try {
+            await xyzToken.addBenificiaries([
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+                "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199",
+            ])
+        } catch (err) {
+            e = err;
+        }
+        expect(e.message.includes("You can add upto 10 benificiary!")).to.equal(true);
     })
 });
