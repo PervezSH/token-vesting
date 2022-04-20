83db2fac8b055b612e1431f675ef19dfb1b1d8fc
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-04-20 22:02:09 +0530

    feat(web-app): add method to add beneficiary ✨

--- frontend/src/components/add-beneficiary/index.tsx
+++ frontend/src/components/add-beneficiary/index.tsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddBeneficiary = () => {
+    const [beneficiaryInput, setBeneficiaryInput] = useState<string>("");
+    const [beneficiaries] = useState<string[]>([]);
+
+    const addBeneficiaryToList = () => {
+        if (beneficiaryInput !== "") {
+            if (beneficiaries.length < 10) beneficiaries.push(beneficiaryInput)
+            else alert("Maximum of 10 address can be added 🤕")
+            setBeneficiaryInput("");
+        }
+    }
+
     return (
         <div>
-            Add Beneficiary
+            <h2>
+                Beneficiary
+            </h2>
+            <p>
+                Addresses where the token will get dispersed evenly, max of 10 addresses only!
+            </p>
+            <div className="input-container">
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    addBeneficiaryToList();
+                }}>
+                    <input type="text" placeholder="Enter beneficiary address"
+                        value={beneficiaryInput}
+                        onChange={(e) => setBeneficiaryInput(e.target.value)}
+                    />
+                </form>
+                <button className="button add-beneficiary" onClick={addBeneficiaryToList}>
+                    Add
+                </button>
+            </div>
+            <div className="beneficiary-list">
+                {beneficiaries.map((item) => (
+                    <div className="beneficiary-list-item" key={item}>
+                        {item}
+                    </div>
+                ))}
+            </div>
+            <button className="button start-vesting" >
+                Start Vesting
+            </button>
         </div>
     )
 }
