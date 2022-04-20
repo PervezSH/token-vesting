f14625b033137beba525bfc6739420dfca24bcea
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-04-20 14:55:46 +0530

    feat(app): add method to connect wallet ✨
    - add method to check if wallet is connected
    - auto connect if we're authorized to access the user's wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,72 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      // @ts-ignore
+      // check if user have metamask or not
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      // if we're authorized to acess the user's wallet
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.log("No authorized account found!");
+      }
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  //Connects my wallet to this site
+  const connectWallet = async () => {
+    try {
+      // @ts-ignore
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask");
+        return;
+      }
+      // If we find MetaMask, ask MetaMask to give access to user's wallet
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected : ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const renderContent = () => {
+    if (!currentAccount) {
+      return (
+        <div>
+          <div className='subheader-text'>
+            Interact with the XYZ token
+          </div>
+          <button className='button connect-wallet' onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        </div>
+      );
+    }
+  }
+
   return (
     <div className="App">
       <div className='container'>
@@ -11,6 +77,7 @@ function App() {
           </div>
         </header>
         <div className='App-body'>
+          {renderContent()}
         </div>
       </div>
     </div>
