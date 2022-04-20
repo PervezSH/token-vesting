29b661288f34fa147093b7e90cc3242a6acfc3fb
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-04-20 12:57:59 +0530

    feat(DApp): initialize react app 🎉

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <header className="App-header">
+          <div className='header-text'>
+            XYZ Token DApp
+          </div>
+        </header>
+        <div className='App-body'>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
